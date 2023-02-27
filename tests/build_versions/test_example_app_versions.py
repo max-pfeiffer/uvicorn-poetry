0b5a308a60fd7693ad662c7f4072451791bff717
Max Pfeiffer <max@maxpfeiffer.ch>
2023-02-27 20:38:26 +0100

    Fixed tests and removed obsolete tests
    Since I dropped the build stages for development image and test executables these became obsolet.

--- tests/build_versions/test_example_app_versions.py
+++ tests/build_versions/test_example_app_versions.py
@@ -7,7 +7,6 @@ from tests.utils import (
 def test_fast_api_multistage_build_versions(
     fast_api_multistage_production_image,
     fast_api_multistage_production_image_json_logging,
-    fast_api_multistage_development_image,
     version,
 ) -> None:
     production_image_components: ImageTagComponents = (
@@ -32,16 +31,6 @@ def test_fast_api_multistage_build_versions(
         production_image_json_logging_components.version
         == production_image_json_logging_version_tag
     )
-
-    development_image_components: ImageTagComponents = (
-        ImageTagComponents.create_from_tag(
-            fast_api_multistage_development_image
-        )
-    )
-    development_image_version_tag = create_version_tag_for_example_images(
-        version, "development-image"
-    )
-    assert development_image_components.version == development_image_version_tag
 
 
 def test_fast_api_singlestage_image_version(
